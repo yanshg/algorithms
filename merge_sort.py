2d46f52d210538fb73446f153ca9994dc897b721
Ervin Yan <Ervin.Yan@veritas.com>
2020-01-19 21:25:30 -0800

    update merge sort

--- merge_sort.py
+++ merge_sort.py
@@ -3,17 +3,14 @@
 def merge_sort(arr):
     l=len(arr)
     if l>1:
-        mid=int(l/2)
-        L=arr[:mid]
-        R=arr[mid:]
+        mid=l//2
+        L,R=arr[:mid],arr[mid:]
         merge_sort(L)
         merge_sort(R)
         merge(arr,L,R)
 
 def merge(arr,arr_left,arr_right):
-    len_left=len(arr_left)
-    len_right=len(arr_right)
-
+    len_left,len_right=len(arr_left),len(arr_right)
     i=j=k=0
     while j<len_left and k<len_right:
         if arr_left[j] <= arr_right[k]:
@@ -33,8 +30,8 @@ def merge(arr,arr_left,arr_right):
         i+=1
 
 if __name__ == '__main__':
-    numbers=[ 21, 14, 6, 51, 23, 7, 25, 23, 22, 33, 6, 44, 23, 15, 51, 76 ]
-    print("numbers: ", numbers)
-    merge_sort(numbers)
-    print("numbers after merge sort: ", numbers)
+    nums=[ 21, 14, 6, 51, 23, 7, 25, 23, 22, 33, 6, 44, 23, 15, 51, 76 ]
+    print("nums:                  ", nums)
+    merge_sort(nums)
+    print("nums after merge sort: ", nums)
 
